c5b71b16ed88d5dd35428e9d394c957f4119471d
NaluFigueira <anachfigueira@gmail.com>
2020-02-22 22:42:33 -0300

    Incluindo nodemon, sucrase e configurando o modo debug

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const routes = require('./routes');
+import express from "express";
+import routes from './routes';
 
 class App {
   constructor(){
@@ -18,4 +18,4 @@ class App {
   }
 }
 
-module.exports = new App().server;
+export default new App().server;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,4 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
@@ -6,4 +6,4 @@ routes.get('/', (req, res) => {
   return res.json({ message: 'Hello World' })
 })
 
-module.exports = routes;
+export default routes;
