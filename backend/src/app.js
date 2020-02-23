67527f308d44bb7a3192b15f367f34178c529335
NaluFigueira <anachfigueira@gmail.com>
2020-02-22 23:05:45 -0300

    Incluindo eslint, prettier e editorconfig para padronização de código

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,19 @@
 import express from "express";
-import routes from './routes';
+import routes from "./routes";
 
 class App {
-  constructor(){
+  constructor() {
     this.server = express();
 
     this.middlewares();
     this.routes();
   }
 
-  middlewares(){
+  middlewares() {
     this.server.use(express.json());
   }
 
-  routes(){
+  routes() {
     this.server.use(routes);
   }
 }
