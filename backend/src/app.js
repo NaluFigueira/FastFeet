711a77a2a241fd3cede0c37c156ebe8750b08eaf
NaluFigueira <anachfigueira@gmail.com>
2020-02-29 23:27:42 -0300

    Configurando o tratamento de exceções com o sentry

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import path from "path";
+import * as Sentry from "@sentry/node";
+import Youch from "youch";
+import "express-async-errors";
 import routes from "./routes";
+import sentryConfig from "./config/sentry";
 
 import "./database";
 
@@ -9,11 +13,15 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       "/files",
@@ -23,6 +31,19 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === "development") {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: "Internal server error" });
+    });
   }
 }
 
---
+++ backend/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: "https://a2871f0b9d2543e8b8519af9c958f61b@sentry.io/3298857",
+};
